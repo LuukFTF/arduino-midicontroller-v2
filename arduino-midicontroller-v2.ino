6898f6613aa7f11bbbcf4a7b18ef273b0b077a85
LuukFTF <lucasvdvegt@gmail.com>
2020-10-12 01:01:50 +0200

    sonar class & midi & update

---
+++ arduino-midicontroller-v2.ino
@@ -0,0 +1,50 @@
+#include "clSonar.h"
+#include "clPotentiometer.h"
+
+// DEFINE PINS
+
+#define TRIGPIN_1 2
+#define ECHOPIN_1 3
+
+#define TRIGPIN_2 4
+#define ECHOPIN_2 5
+
+#define TRIGPIN_3 6
+#define ECHOPIN_3 7
+
+#define TRIGPIN_4 8
+#define ECHOPIN_4 9
+
+#define TRIGPIN_5 10
+#define ECHOPIN_5 11
+
+#define TRIGPIN_6 12
+#define ECHOPIN_6 13
+
+// INIT CLASS INSTANCE
+
+clSonar sonar1(TRIGPIN_1, ECHOPIN_1, 0);
+clSonar sonar2(TRIGPIN_2, ECHOPIN_2, 1);
+clSonar sonar3(TRIGPIN_3, ECHOPIN_3, 2);
+clSonar sonar4(TRIGPIN_4, ECHOPIN_4, 3);
+clSonar sonar5(TRIGPIN_5, ECHOPIN_5, 4);
+clSonar sonar6(TRIGPIN_6, ECHOPIN_6, 5);
+
+// GENERAL SETUP
+
+void setup() {
+  Serial.begin(115200); 
+}
+
+// LOOP
+
+void loop() {
+  sonar1.updateMidi();
+  sonar2.updateMidi();
+  sonar3.updateMidi();
+  sonar4.updateMidi();
+  sonar5.updateMidi();
+  sonar6.updateMidi();
+
+  delay(100); 
+}
---
+++ clSonar.h
@@ -0,0 +1,58 @@
+
+void MIDImessage(uint8_t status, uint8_t data1, uint8_t data2) {
+  Serial.write(status);
+  Serial.write(data1);
+  Serial.write(data2);  
+  
+}
+
+class clSonar {
+  private:
+    int trigPin;
+    int echoPin;
+    int midiControllerChannel;
+    float duration;
+    float distance;
+    float lastVal;
+    float diff = 1;
+  public:
+  clSonar(int trigPin, int echoPin, int midiControllerChannel) {
+    this->trigPin = trigPin;
+    this->echoPin = echoPin;
+    this->midiControllerChannel = midiControllerChannel;
+    init();
+  }
+  void init() {
+    pinMode(trigPin, OUTPUT); 
+    pinMode(echoPin, INPUT); 
+    update();
+    }
+  void update() {
+    digitalWrite(trigPin, LOW); 
+    delayMicroseconds(2); 
+    digitalWrite(trigPin, HIGH); 
+    delayMicroseconds(10); 
+    digitalWrite(trigPin, LOW); 
+    
+    duration = pulseIn(echoPin, HIGH); 
+    distance = (duration*.0343)/2; 
+   }
+  void getValue() {
+    update();
+    return(distance);
+  }
+
+  void updateMidi() {
+    getValue();
+    
+    int valDiff = distance - lastVal;
+
+    if (abs(valDiff) > diff && distance < 50) {
+      int distanceMap = map(distance, 0, 50, 0, 127);
+      MIDImessage(177, midiControllerChannel, distanceMap);
+//      Serial.print("Distance: "); 
+//      Serial.println(distance);
+      lastVal = distance;
+    }
+  }
+};
